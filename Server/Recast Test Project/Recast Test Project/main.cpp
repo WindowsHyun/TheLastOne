395744c117ed574e108fa1f624107a873d33c52c
Ryzen-WindowsHyun <kwon616@gmail.com>
2018-03-21 00:36:35 +0900

    NavMesh 서버 충돌체크를 위한 중간 커밋 두번째   - obj 불러오기 시작점 끝점 설정   - Sample_SoloMesh 를 분석하여 원하는 위치를 선택후 노드가 나올 수 있게 작업 결과를 표출 해야한다.

--- Server/Recast Test Project/Recast Test Project/main.cpp
+++ Server/Recast Test Project/Recast Test Project/main.cpp
@@ -2,6 +2,7 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include "Recast.h"
+//#include "Sample_SoloMesh.h"
 #include "MeshLoaderObj.h"
 #include "InputGeom.h"
 
@@ -15,6 +16,17 @@ int main() {
 	rcMeshLoaderObj mesh;
 	InputGeom* geom = 0;
 	rcContext ctx;
+	//----------------------------------------
+	unsigned char* m_triareas;
+	rcHeightfield* m_solid;
+	rcCompactHeightfield* m_chf;
+	rcContourSet* m_cset;
+	rcPolyMesh* m_pmesh;
+	rcConfig m_cfg;
+	rcPolyMeshDetail* m_dmesh;
+	//----------------------------------------
+
+
 
 	//------------------------
 	// 유니티 기준 플레이어 위치 x, z
@@ -25,11 +37,41 @@ int main() {
 	int tx = 328;
 	int ty = 801;
 	//------------------------
+	float rayStart[3]{ 1009, 29.99451f, 1150 };	// x, y, z
+	float rayEnd[3]{ 328, 29.99451f, 801 };		// x, y, z
+	float hitTime;
 
 	std::cout << mesh.load("./navmesh/InGame.obj") << std::endl;
 
 	geom = new InputGeom;
 	geom->loadMesh("./navmesh/InGame.obj");
 
+	const float* bmin = geom->getMeshBoundsMin();
+	const float* bmax = geom->getMeshBoundsMax();
+	const float* verts = geom->getMesh()->getVerts();
+	const int nverts = geom->getMesh()->getVertCount();
+	const int* tris = geom->getMesh()->getTris();
+	const int ntris = geom->getMesh()->getTriCount();
+
+	std::cout << "bmin : " << bmin << ", bmax : " << bmax << std::endl;
+
+	m_solid = rcAllocHeightfield();
+	m_triareas = new unsigned char[ntris];
+	memset(m_triareas, 0, ntris * sizeof(unsigned char));
+	m_chf = rcAllocCompactHeightfield();
+
+	//Sample_SoloMesh.cpp 에서  397번째 라인부터 분석을 하면 된다.
+
+	const ConvexVolume* vols = geom->getConvexVolumes();
+
+
+	bool hit = geom->raycastMesh(rayStart, rayEnd, hitTime);
+
+	std::cout << hit << std::endl;
+	/*
+	duDebugDrawTriMesh(&dd, m_geom->getMesh()->getVerts(), m_geom->getMesh()->getVertCount(),
+	m_geom->getMesh()->getTris(), m_geom->getMesh()->getNormals(), m_geom->getMesh()->getTriCount(), 0, 1.0f);
+	*/
+	std::cout << geom->getMesh() << std::endl;
 
 }
