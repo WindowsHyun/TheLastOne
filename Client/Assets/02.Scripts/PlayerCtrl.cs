2bde1668196f74d58a7fe1ddf4c58eb5b1f53514
DESKTOP-V0U6HCR\shtke <shtkeh77@naver.com>
2018-05-14 01:06:21 +0900

    플레이어 이동 치트 수정

--- Client/Assets/02.Scripts/PlayerCtrl.cs
+++ Client/Assets/02.Scripts/PlayerCtrl.cs
@@ -440,7 +440,7 @@ public class PlayerCtrl : PlayerVehicleCtrl
             if (Input.GetKeyDown(KeyCode.Alpha3))
             {
                 navagent.enabled = false;
-                tr.transform.position = new Vector3(776.9319f, 30.00061f, 441.4027f);
+                tr.transform.position = new Vector3(349.1992f, 60.06981f, 376.0149f);
                 navagent.enabled = true;
             }
             else if (Input.GetKeyDown(KeyCode.Alpha4))
@@ -452,7 +452,7 @@ public class PlayerCtrl : PlayerVehicleCtrl
             else if (Input.GetKeyDown(KeyCode.Alpha5))
             {
                 navagent.enabled = false;
-                tr.transform.position = new Vector3(484.5258f, 30.00061f, 2534.864f);
+                tr.transform.position = new Vector3(526.118f, 50.04224f, 2705.361f);
                 navagent.enabled = true;
             }
             else if (Input.GetKeyDown(KeyCode.Alpha6))
@@ -464,15 +464,17 @@ public class PlayerCtrl : PlayerVehicleCtrl
             else if (Input.GetKeyDown(KeyCode.Alpha7))
             {
                 navagent.enabled = false;
-                tr.transform.position = new Vector3(1651.698f, 30.00061f, 1714.017f);
+                tr.transform.position = new Vector3(1545.017f, 59.95751f, 904.9451f);
                 navagent.enabled = true;
             }
-            else if (Input.GetKeyDown(KeyCode.Alpha7))
+            else if (Input.GetKeyDown(KeyCode.Alpha8))
             {
                 navagent.enabled = false;
-                tr.transform.position = new Vector3(1523.183f, 60.00122f, 940.6536f);
+                tr.transform.position = new Vector3(1008.397f, 30.06981f, 1553.832f);
                 navagent.enabled = true;
             }
+
+
             else if (Input.GetKeyDown(KeyCode.Alpha0))
             {
                 // Navmesh ON/OFF
